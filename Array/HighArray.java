bdfbb39d4603d6245ec1a3acdf705130f4a131cc
iridwil <r.magomed.i@mail.ru>
2017-06-07 18:20:12 +0300

    07.06.2017
    Работа с массивами

---
+++ Array/HighArray.java
@@ -0,0 +1,48 @@
+class HighArray {
+	private long[] a;
+	private int nElems;
+
+	public HighArray(int max) {
+		a = new long[max];
+		nElems = 0;
+	}
+
+	public boolean find(long searchkey) {
+		int j;
+		for(j = 0; j < nElems; j++) 
+			if(a[j] == searchkey) 
+				break;
+		if(j == nElems) 
+			return false;
+		else 
+			return true; 
+	}
+
+	public void insert(long value) {
+		a[nElems] = value;
+		nElems++;
+	}
+
+	public boolean delete(long value) {
+		int j;
+		for(j = 0; j < nElems; j++) 
+			if(value == a[j])
+				break;
+		if(j == nElems) 
+			return false;
+		else {
+			for(int k = j; k < nElems; k++)
+				a[k] = a[k+1];
+			nElems--;
+			return true;
+		}
+	}
+
+	public void display() {
+		for(int j = 0; j < nElems; j++) 
+			System.out.print(a[j] + " ");
+		System.out.println("");
+	}
+}
+
+
